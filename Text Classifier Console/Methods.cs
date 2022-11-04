2c6fce6eb6560442bc8492ce9333432a3099f786
dennis kenneth <dennyify123@gmail.com>
2022-11-04 13:49:27 +0000

    Add project files.

---
+++ Text Classifier Console/Methods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Classifier_Console
+{
+    
+     class Methods
+    {
+        private string requestTxt;
+        private string endpoint;
+        static readonly HttpClient client = new HttpClient();
+
+        public Methods(string txt)
+        {
+            this.requestTxt = txt;
+            this.endpoint = "https://text-classification-bart.ai-sandbox.4th-ir.io/api/v1/classify";
+            
+        }
+
+        public async Task classify()
+        {
+            var requestObj = new { text = requestTxt };
+
+            try
+            {
+                HttpResponseMessage response = await client.PostAsJsonAsync(endpoint, requestObj);
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine("Success");
+                var responseString = await response.Content.ReadFromJsonAsync<SuccessResponse>();
+                var result = responseString.label;
+                //return result
+                Console.WriteLine(result);
+            }catch(Exception e) {
+                Console.WriteLine("\nException Caught!");
+               Console.WriteLine(e.Message);
+                //return e.Message;
+            }
+            
+        }
+    }
+
+    public class SuccessResponse
+    {
+        public string label { get; set; }
+        //public string conversion_text { get; set; }
+    }
+}
---
+++ Text Classifier Console/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.IO;
+
+
+namespace Text_Classifier_Console 
+{
+    class Program
+    {
+        static Methods Api;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to 4th IR's Text Classifier");
+            Console.WriteLine("PLease Enter some text");
+            string consoleTxt = Console.ReadLine();
+
+            if (consoleTxt != null && consoleTxt.GetType() == typeof(string))
+            {
+                Api = new Methods(consoleTxt);
+                await Api.classify();
+
+                
+            }else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            
+        }
+    }
+}
